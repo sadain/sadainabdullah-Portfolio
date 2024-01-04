4b16fd5e0d40c01ca6f63a740e9ea067c28e1c26
sadain <nsasadain@gmail.com>
2024-01-04 16:16:56 +0530

    Fix Experince Content

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -51,7 +51,7 @@ const Experience = () => {
                   <li id="tflex" className="mb-12">
                     <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                       <div className="bg-white/20 absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-xl transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-transparent lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                      <header className="z-10 mb-2 mt-1 text-sm font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="OCT to Present">OCT — Present</header>
+                      <header className="z-10 mb-2 mt-1 text-sm font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="OCT to Present">SEP — Present</header>
                       <div className="z-10 sm:col-span-6">
                         <h3 className="font-medium leading-snug text-slate-200">
                           <div>
